9001aca99287b74d961d32f07a821c40759fb369
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-19 16:41:08 +0700

    modify the table

--- MySQL/belajar.sql
+++ MySQL/belajar.sql
@@ -7,3 +7,24 @@ CREATE TABLE barang(
     harga INT,
     jumlah INT
 )ENGINE = InnoDB;
+
+-- Melihat struktur Table
+DESCRIBE barang;
+DESC barang;
+SHOW CREATE TABLE barang;
+
+-- Mengubah Table
+ALTER TABLE barang
+ADD COLUMN deskripsi TEXT;
+
+ALTER TABLE barang
+ADD COLUMN salah TEXT; --menambah kolumn
+
+ALTER TABLE barang
+drop COLUMN salah; -- menghapus kolom
+
+ALTER TABLE barang
+MODIFY nama VARCHAR(200) AFTER deskripsi;
+
+ALTER TABLE barang
+MODIFY nama VARCHAR(200) FIRST;
