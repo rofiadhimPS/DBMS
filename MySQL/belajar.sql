5b57782a6536894867e643a171da9f73043b7a2f
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-01-19 18:27:01 +0700

    truncate and drop the table

--- MySQL/belajar.sql
+++ MySQL/belajar.sql
@@ -1,4 +1,4 @@
--- Active: 1737277784953@@127.0.0.1@3306@belajar_mysql
+-- Active: 1737277784953@@127.0.0.1@3306
 SHOW ENGINES; -- Melihat engine
 SHOW TABLES; -- melihat tabel
 CREATE TABLE barang(
@@ -28,3 +28,32 @@ MODIFY nama VARCHAR(200) AFTER deskripsi;
 
 ALTER TABLE barang
 MODIFY nama VARCHAR(200) FIRST;
+
+ALTER TABLE barang
+MODIFY ID INT NOT NULL; -- Not Null for id
+
+ALTER TABLE barang
+MODIFY nama VARCHAR(200) NOT NULL;
+
+SHOW CREATE TABLE barang;
+
+ALTER Table barang
+MODIFY jumlah INT NOT NULL DEFAULT 0;
+
+ALTER TABLE barang
+MODIFY harga INT NOT NULL DEFAULT 0;
+
+ALTER TABLE barang
+ADD waktu_dibuat TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+
+INSERT INTO barang (id, nama) VALUES (1, "Apel");
+
+SELECT * FROM barang;
+
+-- Membuat Ulang Table
+
+TRUNCATE barang;
+
+SHOW TABLES;
+
+DROP TABLE barang; -- Menghapus table barang
