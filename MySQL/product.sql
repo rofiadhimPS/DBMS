6801a4c82f2ece2641cea2bb17d2d480bf52b6c9
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-02-05 10:24:35 +0700

    where clause

--- MySQL/product.sql
+++ MySQL/product.sql
@@ -37,3 +37,13 @@ ALTER TABLE products
 ADD PRIMARY KEY (id);
 
 DESCRIBE products;
+
+SELECT * FROM products WHERE quantity = 100;
+
+SELECT * FROM products WHERE price = 15000;
+
+SELECT * FROM products WHERE id = 'P0001';
+
+SELECT * FROM products WHERE name = 'Mie Ayam Bakso';
+
+
