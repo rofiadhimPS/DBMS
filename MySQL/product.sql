94d054f02a99eb433c745a9b7a483e4c98dff484
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-02-05 13:53:31 +0700

    numerical function

--- MySQL/product.sql
+++ MySQL/product.sql
@@ -148,6 +148,13 @@ SELECT * FROM products ORDER BY id LIMIT 5, 5;
 
 SELECT DISTINCT category FROM products;
 
+SELECT 10, 10, 10*10 AS Hasil;
+
+SELECT id, name, price, price DIV 1000 AS 'Price in K' FROM products;
+
+SELECT id, COS(price), SIN(price), TAN(price) FROM products;
+
+SELECT id, name, price FROM products WHERE price DIV 1000 > 15;
 
 
 
