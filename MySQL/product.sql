4526e8d6bae1f51131da70dd7ca4d07db3059c74
rofiadhimPS <muhammadrofiadhim@gmail.com>
2025-02-05 12:00:34 +0700

    update query

--- MySQL/product.sql
+++ MySQL/product.sql
@@ -46,4 +46,30 @@ SELECT * FROM products WHERE id = 'P0001';
 
 SELECT * FROM products WHERE name = 'Mie Ayam Bakso';
 
+DESCRIBE products;
+
+ALTER TABLE products
+ADD COLUMN category ENUM('Makanan', 'Minuman', 'Lain-lain')
+AFTER name;
+
+SELECT * FROM products;
+
+SELECT * FROM products WHERE id = 'P0001';
+
+UPDATE products
+SET category = 'Makanan'
+WHERE id = 'P0001';
+
+UPDATE products
+SET category = 'Makanan',
+    description = 'Mie Ayam Original + Ceker'
+WHERE id = 'P0003';
+
+
+UPDATE products
+SET price = price + 5000
+WHERE id = 'P0005';
+
+
+
 
